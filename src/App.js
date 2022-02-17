0253bf5563ed8ca144570c4a10306de71fe3da3e
melissa-morgan <melissamorganc@gmail.com>
2022-02-17 13:13:23 -0500

    add pet details and modal for contact

--- src/App.js
+++ src/App.js
@@ -20,162 +20,161 @@ import UserProfile from './components/UserProfile/UserProfile';
 import HowItWorks from './components/HowItWorks/HowItWorks';
 
 function App() {
-  const [pets, setPets] = useState([]);
-  const [filtered, setFiltered] = useState([]);
+	const [pets, setPets] = useState([]);
+	const [filtered, setFiltered] = useState([]);
 
-  const url = `https://petfindr-api.herokuapp.com/pets/`;
-  const [errMsg, setErrMsg] = useState('');
+	const url = `https://petfindr-api.herokuapp.com/pets/`;
+	const [errMsg, setErrMsg] = useState('');
 
-  const getAllPets = async () => {
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      setPets(data);
-      setFiltered(data);
-    } catch (error) {
-      setErrMsg(error);
-    }
-  };
+	const getAllPets = async () => {
+		try {
+			const response = await fetch(url);
+			const data = await response.json();
+			setPets(data);
+			setFiltered(data);
+		} catch (error) {
+			setErrMsg(error);
+		}
+	};
 
-  useEffect(() => {
-    getAllPets();
-  }, []);
+	useEffect(() => {
+		getAllPets();
+	}, []);
 
-  const [petStatus, setPetStatus] = useState({});
+	const [petStatus, setPetStatus] = useState({});
 
-  const navigate = useNavigate();
-  const [loggedIn, setLoggedIn] = useState(
-    localStorage.getItem('token') ? true : false
-  );
-  const [userInfo, setUserInfo] = useState(null);
+	const navigate = useNavigate();
+	const [loggedIn, setLoggedIn] = useState(
+		localStorage.getItem('token') ? true : false
+	);
+	const [userInfo, setUserInfo] = useState(null);
 
-  // Setup useLocation for Report Pet Link with a Ternary
-  const location = useLocation();
-  const [locationReportPet, setLocationReportPet] = useState(false);
-  useEffect(() => {
-    if (location.pathname === '/report-pet') {
-      setLocationReportPet(true);
-    } else {
-      setLocationReportPet(false);
-    }
-  }, [location.pathname]);
+	// Setup useLocation for Report Pet Link with a Ternary
+	const location = useLocation();
+	const [locationReportPet, setLocationReportPet] = useState(false);
+	useEffect(() => {
+		if (location.pathname === '/report-pet') {
+			setLocationReportPet(true);
+		} else {
+			setLocationReportPet(false);
+		}
+	}, [location.pathname]);
 
-  // Handle the logged in state
-  const handleSetLoggedIn = (token) => {
-    localStorage.setItem('token', token);
-    setLoggedIn(true);
-    return;
-  };
+	// Handle the logged in state
+	const handleSetLoggedIn = (token) => {
+		localStorage.setItem('token', token);
+		setLoggedIn(true);
+		return;
+	};
 
-  // Get the User Info
-  const getUserInfo = async () => {
-    try {
-      const response = await fetch(API_URL + 'users/me/', {
-        headers: {
-          Authorization: `Token ${localStorage.getItem('token')}`,
-        },
-      });
-      if (response.status === 200) {
-        const data = await response.json();
-        setUserInfo(data);
-      } else {
-        setUserInfo(null);
-        setLoggedIn(false);
-        localStorage.clear();
-      }
-    } catch (error) {}
-    return;
-  };
+	// Get the User Info
+	const getUserInfo = async () => {
+		try {
+			const response = await fetch(API_URL + 'users/me/', {
+				headers: {
+					Authorization: `Token ${localStorage.getItem('token')}`,
+				},
+			});
+			if (response.status === 200) {
+				const data = await response.json();
+				setUserInfo(data);
+			} else {
+				setUserInfo(null);
+				setLoggedIn(false);
+				localStorage.clear();
+			}
+		} catch (error) {}
+		return;
+	};
 
-  // Handle the Logout Functionality
-  const handleLogout = async () => {
-    try {
-      const response = await fetch(API_URL + 'token/logout/', {
-        method: 'POST',
-        body: JSON.stringify(localStorage.getItem('token')),
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Token ${localStorage.getItem('token')}`,
-        },
-      });
-      if (response.status === 204) {
-        setUserInfo(null);
-        setLoggedIn(false);
-        localStorage.clear();
-        navigate('/');
-      }
-    } catch (error) {}
-    return;
-  };
+	// Handle the Logout Functionality
+	const handleLogout = async () => {
+		try {
+			const response = await fetch(API_URL + 'token/logout/', {
+				method: 'POST',
+				body: JSON.stringify(localStorage.getItem('token')),
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Token ${localStorage.getItem('token')}`,
+				},
+			});
+			if (response.status === 204) {
+				setUserInfo(null);
+				setLoggedIn(false);
+				localStorage.clear();
+				navigate('/');
+			}
+		} catch (error) {}
+		return;
+	};
 
-  // If the user is logged in, run getUserInfo();
-  useEffect(() => {
-    if (loggedIn) {
-      getUserInfo();
-    }
-  }, [loggedIn]);
+	// If the user is logged in, run getUserInfo();
+	useEffect(() => {
+		if (loggedIn) {
+			getUserInfo();
+		}
+	}, [loggedIn]);
 
-  return (
-    <div className='App'>
-      <div className='paw-one'>
-        <FaPaw />
-      </div>
-      <div className='paw-two'>
-        <FaPaw />
-      </div>
-
-      <header>
-        <Navigation
-          setPetStatus={setPetStatus}
-          loggedIn={loggedIn}
-          handleLogout={handleLogout}
-          userInfo={userInfo}
-        />
-      </header>
-      <main>
-        <ReportPet locationReportPet={locationReportPet} loggedIn={loggedIn} />
-        <Routes>
-          <Route
-            path='/'
-            element={<Home loggedIn={loggedIn} setPetStatus={setPetStatus} />}
-          />
-          <Route
-            path='/login'
-            element={<Login handleSetLoggedIn={handleSetLoggedIn} />}
-          />
-          <Route path='/register' element={<Register />} />
-          <Route path='/report-pet' element={<AddPets loggedIn={loggedIn} />} />
-          <Route
-            path='/dashboard'
-            element={
-              <PetDashboard
-                petStatus={petStatus}
-                setPetStatus={setPetStatus}
-                pets={pets}
-                filtered={filtered}
-                setFiltered={setFiltered}
-              />
-            }
-          />
-          <Route path='/pets/:id' element={<PetDetails />}></Route>
-          <Route
-            path='/user-profile'
-            element={
-              <UserProfile
-                loggedIn={loggedIn}
-                handleLogout={handleLogout}
-                userInfo={userInfo}
-              />
-            }
-          />
-          <Route path='/howitworks' element={<HowItWorks />}></Route>
-        </Routes>
-      </main>
-      <footer>
-        <Footer setPetStatus={setPetStatus} />
-      </footer>
-    </div>
-  );
+	return (
+		<div>
+			<div className='paw-one'>
+				<FaPaw />
+			</div>
+			<div className='paw-two'>
+				<FaPaw />
+			</div>
+			<header>
+				<Navigation
+					setPetStatus={setPetStatus}
+					loggedIn={loggedIn}
+					handleLogout={handleLogout}
+					userInfo={userInfo}
+				/>
+			</header>
+			<main>
+				<ReportPet locationReportPet={locationReportPet} loggedIn={loggedIn} />
+				<Routes>
+					<Route
+						path='/'
+						element={<Home loggedIn={loggedIn} setPetStatus={setPetStatus} />}
+					/>
+					<Route
+						path='/login'
+						element={<Login handleSetLoggedIn={handleSetLoggedIn} />}
+					/>
+					<Route path='/register' element={<Register />} />
+					<Route path='/report-pet' element={<AddPets loggedIn={loggedIn} />} />
+					<Route
+						path='/dashboard'
+						element={
+							<PetDashboard
+								petStatus={petStatus}
+								setPetStatus={setPetStatus}
+								pets={pets}
+								filtered={filtered}
+								setFiltered={setFiltered}
+							/>
+						}
+					/>
+					<Route path='/pets/:id' element={<PetDetails />}></Route>
+					<Route
+						path='/user-profile'
+						element={
+							<UserProfile
+								loggedIn={loggedIn}
+								handleLogout={handleLogout}
+								userInfo={userInfo}
+							/>
+						}
+					/>
+					<Route path='/howitworks' element={<HowItWorks />}></Route>
+				</Routes>
+			</main>
+			<footer>
+				<Footer setPetStatus={setPetStatus} />
+			</footer>
+		</div>
+	);
 }
 
 export default App;
--- src/components/PetDetails/PetDetails.js
+++ src/components/PetDetails/PetDetails.js
@@ -2,10 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styles from './PetDetails.module.css';
 import moment from 'moment';
+import Modal from 'react-modal';
+// Modal error message
+Modal.setAppElement('#root')
 
 function PetDetails(props) {
 	const [pet, setPet] = useState(null);
 	const { id } = useParams();
+  const [modalIsOpen, setModalIsOpen] = useState(false)
 
 	async function getPet() {
 		const url = `https://petfindr-api.herokuapp.com/pets/${id}`;
@@ -34,9 +38,9 @@ function PetDetails(props) {
 					<h3 className={styles.name}>{pet.name}</h3>
 					<h4 className={styles.status}>Status: {pet.status}</h4>
 				</div>
-					<div className={styles.imgDiv}>
-						<img className={styles.img} src={pet.photo} alt={pet.name} />
-					</div>
+				<div className={styles.imgDiv}>
+					<img className={styles.img} src={pet.photo} alt={pet.name} />
+				</div>
 				<h3>Pet Descriptors</h3>
 				<hr />
 				<ul>
@@ -53,7 +57,43 @@ function PetDetails(props) {
 					<li>Last Location: {pet.location}</li>
 					<li>Date: {moment(pet.reported_time).format('LLL')}</li>
 				</ul>
-        <button className={styles.contact}>Contact</button>
+				<button onClick={() => setModalIsOpen(true)} className={styles.contact}>
+					Contact
+				</button>
+				<div className={styles.modalDiv}>
+					<Modal
+						isOpen={modalIsOpen}
+						onRequestClose={() => setModalIsOpen(false)}
+						style={{
+							overlay: {
+								position: 'fixed',
+								top: 0,
+								left: 0,
+								right: 0,
+								bottom: 0,
+								backgroundColor: 'rgba(255, 255, 255, 0.75)',
+							},
+							content: {
+								position: 'absolute',
+								top: '20%',
+								left: '30%',
+								right: '30%',
+								bottom: '20%',
+								border: '1px solid #ccc',
+								background: '#fff',
+								overflow: 'auto',
+								WebkitOverflowScrolling: 'touch',
+								borderRadius: '4px',
+								outline: 'none',
+								padding: '20px',
+							},
+						}}>
+						<h2 className={styles.modalTitle}>Contact Info</h2>
+						<p className={styles.modalItem}>{pet.phone_number}</p>
+						<p className={styles.modalItem}>{pet.owner_email}</p>
+						<button onClick={() => setModalIsOpen(false)}>Close</button>
+					</Modal>
+				</div>
 			</div>
 		</div>
 	);
