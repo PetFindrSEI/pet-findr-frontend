b59e1bd113f8d75fdd2063b7626800708aaeea90
melissa-morgan <melissamorganc@gmail.com>
2022-02-17 09:55:44 -0500

    how it works, login, details

--- src/components/HowItWorks/HowItWorks.js
+++ src/components/HowItWorks/HowItWorks.js
@@ -1,16 +1,33 @@
 import React from 'react';
+import styles from './HowItWorks.module.css'
 
 function HowItWorks(props) {
     return (
-        <div>
-            <h3>Create a free listing</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae dolorum, distinctio quas eius quo excepturi omnis quisquam, quibusdam impedit a dignissimos nostrum modi ducimus, fuga alias assumenda rem illum dolores.</p>
+        <div className={styles.card}>
+            <h2>How it works!</h2>
+            <h3 className={styles.subTitle}>Create a free listing</h3>
+            <p className={styles.description}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+                dolorum, distinctio quas eius quo excepturi omnis quisquam, quibusdam
+                impedit a dignissimos nostrum modi ducimus, fuga alias assumenda rem
+                illum dolores.
+            </p>
             <br />
-            <h3>Get in contact</h3>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos neque sint alias at molestias cupiditate obcaecati. Facilis cumque perferendis quidem molestiae, dignissimos iste quibusdam ipsam temporibus et odit officia architecto?</p>
+            <h3 className={styles.subTitle}>Get in contact</h3>
+            <p className={styles.description}>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos neque
+                sint alias at molestias cupiditate obcaecati. Facilis cumque
+                perferendis quidem molestiae, dignissimos iste quibusdam ipsam
+                temporibus et odit officia architecto?
+            </p>
             <br />
-            <h3>Pet Reunion</h3>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia iste quaerat voluptatibus veniam fugiat iusto voluptatum quis iure vel ipsam laudantium consequuntur corporis doloremque, porro obcaecati, nesciunt expedita sequi aliquid.</p>
+            <h3 className={styles.subTitle}>Pet Reunion</h3>
+            <p className={styles.description}>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia iste
+                quaerat voluptatibus veniam fugiat iusto voluptatum quis iure vel
+                ipsam laudantium consequuntur corporis doloremque, porro obcaecati,
+                nesciunt expedita sequi aliquid.
+            </p>
         </div>
     );
 }
